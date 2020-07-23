ed670d35a9da97517d5c4cdacd0d79068f8468c8
Nicola Horbach <44953799+NicolaHorbach@users.noreply.github.com>
2020-07-23 21:25:22 +0300

    .js + .json codestyle updated

--- gulpfile.js
+++ gulpfile.js
@@ -20,8 +20,10 @@ gulp.task("css", function() {
 gulp.task("server", function() {
   server.init({
     server: "source/",
-    notify: false, open: true,
-    cors: true, ui: false
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
   });
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/*.html").on("change", server.reload);
