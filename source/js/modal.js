6ed287a0903b685d90a67d90df065f4c5024b2d8
Nicola Horbach <44953799+NicolaHorbach@users.noreply.github.com>
2020-08-25 01:04:17 +0300

    hideClassname

--- source/js/modal.js
+++ source/js/modal.js
@@ -2,11 +2,11 @@ const triggersElements = document.querySelectorAll('[data-trigger]');
 const modalElement = document.querySelector('[data-triggeredBy]');
 const modalBackdrop = document.querySelector('.modal-backdrop')
 const showClassname = 'is-opened';
-const bodyBlackout = 'is-hidden';
+const hideClassname = 'is-hidden';
 
 function showModal () {
   modalElement.classList.toggle(showClassname);
-  modalBackdrop.classList.toggle(bodyBlackout);
+  modalBackdrop.classList.toggle(hideClassname);
 }
 
 triggersElements.forEach(el => {
